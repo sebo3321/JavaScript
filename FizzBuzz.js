0f7eaa29314646ad073fe04c82731645864281f8
sebo3321 <68596263+sebo3321@users.noreply.github.com>
2022-10-17 11:20:04 +0100

    Create FizzBuzz.js

---
+++ FizzBuzz.js
@@ -0,0 +1,6 @@
+for(let number = 0; number < 101; number ++){
+  if(number%15 === 0) {console.log("FizzBuzz");}
+  else if(number%3===0) {console.log("Fizz");}
+  else if(number%5===0) {console.log("Buzz");}
+  else {console.log(number);}
+}
